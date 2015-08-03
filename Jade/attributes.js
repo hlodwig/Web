79b9395e65a387219515bc904e41c13e5c81fc23
Luis Rodriguez <iec.luis.rodriguez@gmail.com>
2015-08-02 23:09:38 -0500

    Agregando atributos.

---
+++ Jade/attributes.js
@@ -0,0 +1,7 @@
+function template(locals) {
+var buf = [];
+var jade_mixins = {};
+var jade_interp;
+
+buf.push("<div id=\"content\" class=\"main\"><a href=\"www.instagram.com/luisgotbars\" title=\"Instagram de Luis\" taget=\"_blank\">Link hacia Instagram de Luis</a><form action=\"/login\"><button type=\"submit\" value=\"save\"></button></form></div><div class=\"hero-unit\">Texto debajo de  división :D.</div>");;return buf.join("");
+}
